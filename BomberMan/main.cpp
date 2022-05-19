e0f6f2a6a852b6ce92a9778f70708ec240e32e82
Oscar Maia <oscarfgmaia@gmail.com>
2022-05-18 22:00:08 -0300

    first try to put sprites in the game!

--- BomberMan/main.cpp
+++ BomberMan/main.cpp
@@ -6,62 +6,101 @@
 
 //------------------------------------------------------------------------------------------
 typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
-const int kTileSize = 32;
+const int kTileSize = 64;
 
 class Bomb {
 public:
     Bomb() {}
     Bomb(Color color, Vector2 position) {
         this->color = color;
-        this->bomb_position = position;
+        this->position = position;
     }
     float timer = 60;
     Color color;
-    Vector2 bomb_position;
+    Vector2 position;
+};
 
-    bool shouldExplode() {
-        if (timer <= 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
+class Obstacle {
+public:
+    Color color = BROWN;
+    Vector2 position;
+    bool collision = true;
+    Obstacle() {
+
+    }
+    Obstacle(float x, float y) {
+        this->position.x = position.x;
+        this->position.y = position.y;
     }
 };
 
 class Player {
 public:
+    Texture2D characterSprite = LoadTexture("textures/character.png");
+    Rectangle characterRec;
     Vector2 position;
-    Player() {}
+    Player() {
+        characterRec.height = characterSprite.height / 4;
+        characterRec.width= characterSprite.width/ 8;
+        characterRec.x = 0;
+        characterRec.y = 0;
+    }
     Player(Vector2 position) { this->position = position; };
 
     void MoveUp() { position.y -= 1; }
     void MoveDown() { position.y += 1; }
     void MoveRight() { position.x += 1; }
     void MoveLeft() { position.x -= 1; }
-    Vector2 GetPosition() { return position; }
+    Vector2 GetPositionFixedForSprite() { 
+        Vector2 temp = position;
+        temp.x = temp.x * 64;
+        temp.y = temp.y * 64;
+        return temp; 
+    }
+    Vector2 GetPosition() {
+        return position;
+    }
 };
 
 class Render {
 public:
     Render() {}
     void RenderPlayer(Player& player, Color color) {
-        auto sprite = Rectangle{
-            player.position.x * kTileSize, player.position.y * kTileSize, kTileSize, kTileSize };
-        DrawRectangleRec(sprite, color);
+        DrawTextureRec(player.characterSprite, player.characterRec, player.GetPositionFixedForSprite(), WHITE);
+        DrawText(std::to_string(player.position.x).c_str(), 300, 380, 20, RED);
+        DrawText(std::to_string(player.position.y).c_str(), 300, 400, 20, RED);
     }
     void RenderBomb(Bomb& bomb) {
-        DrawCircle(bomb.bomb_position.x * kTileSize, bomb.bomb_position.y * kTileSize, 16, bomb.color);
+        DrawCircle(bomb.position.x * kTileSize, bomb.position.y * kTileSize, 32, bomb.color);
+    }
+    void RenderObstacles(Obstacle& obstacle) {
+        DrawRectangle(obstacle.position.x * kTileSize, obstacle.position.y * kTileSize, 64, 64, obstacle.color);
     }
 };
+
+class Map {
+public:
+    std::string mapName;
+    Obstacle obstacles;
+    Map(std::string name) {
+        this->mapName = name;
+        std::list<Obstacle> obstacles;
+        if (GetScreenWidth() % 2 == 0) {
+            for(int i = 0; i < GetScreenWidth()/64; i++) {
+                obstacles.push_back(Obstacle((float)i,0));
+            }
+        }
+    }
+};
+
 class Game {
 public:
     Game() {}
     Player player1;
-    Player player2;
+    //Player player2;
+    std::list<Obstacle> *obstacles;
     Render render;
     std::list<Bomb> bombList;
-    std::string bombSize;
 
     void GameUpdate() {
         //---------DELTA TIME ---------
@@ -90,7 +129,7 @@ public:
         if (IsKeyPressed(KEY_RIGHT_SHIFT)) {
             bombList.push_back(Bomb(RED, player1.GetPosition()));
         }
-
+        /*
         //---------PLAYER 2 INPUTS-------------
         if (IsKeyPressed(KEY_W)) {
             player2.MoveUp();
@@ -110,28 +149,33 @@ public:
         if (IsKeyPressed(KEY_E)) {
             bombList.pop_front();
         }
+        */
     }
     void DrawGame() {
         render.RenderPlayer(player1, BLACK);
-        render.RenderPlayer(player2, PURPLE);
+      //  render.RenderPlayer(player2, PURPLE);
 
         for (Bomb& bombs : bombList) {
             render.RenderBomb(bombs);
             DrawText(TextFormat("Y: %2.0f", bombs.timer), 300, 360, 20, RED);
-            bombSize = std::to_string(bombList.size());
-            DrawText(bombSize.c_str(), 300, 380, 20, RED);
+            DrawText(std::to_string(bombList.size()).c_str(), 300, 380, 20, RED);
         }
     }
 };
 
 int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1600;
+    const int screenHeight = 960;
     GameScreen current_screen = LOGO;
 
     InitWindow(screenWidth, screenHeight, "BomberMan");
     SetTargetFPS(60);
     Game game;
+    Map map("first");
+    game.player1.position.x = 24;
+    game.player1.position.y = 7;
+    //game.player2.position.x = 0;
+   // game.player2.position.y = 7;
     int framesCounter = 0;
     while (!WindowShouldClose()) {
         switch (current_screen) {
@@ -166,6 +210,10 @@ int main(void) {
         case ENDING:
             if (IsKeyPressed(KEY_ENTER)) {
                 current_screen = LOGO;
+                game.player1.position.x = 24;
+                game.player1.position.y = 7;
+               // game.player2.position.x = 0;
+               // game.player2.position.y = 7;
             }
             break;
         default:
